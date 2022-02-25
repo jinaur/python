345618c08fc6d60348e6bce2c7486b2a14281fec
jinaur <50763720+jinaur@users.noreply.github.com>
2022-02-25 20:24:20 +0900

    .

--- 7-8_사과나무.py
+++ 7-8_사과나무.py
@@ -6,7 +6,7 @@ for i in range(0, n) :
 distance = 0 # 양옆으로 가는 이동거리, 처음엔 양옆으로 가지않게 0
 apple_count = 0 # 사과 개수
 
-def tree(distance, m, l) :
+def apple_tree(distance, m, l) :
 	global apple_count
 	for i in range(distance, l) :
 		for j in range(0, distance) : # 중앙에서 부터 양옆으로가 사과 개수를 셈
@@ -15,8 +15,8 @@ def tree(distance, m, l) :
 		apple_count += apple_list[i][n//2] # 중앙 사과
 		distance += m
 
-tree(0, 1, n//2)
-tree(n//2, -1, n)
+apple_tree(0, 1, n//2)
+apple_tree(n//2, -1, n)
 print(apple_count)
 
 
