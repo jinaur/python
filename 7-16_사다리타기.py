4b2f2e061cd74cf299f02f0fb68ec79e76b329e3
jinaur <50763720+jinaur@users.noreply.github.com>
2022-03-18 20:08:21 +0900

    .

---
+++ 7-16_사다리타기.py
@@ -0,0 +1,24 @@
+dx = [-1, 1]
+l = []
+for i in range(0, 10) :
+	a = list(map(int, input().split()))
+	l.append(a)
+
+def ladder(y, x, l) :
+    global check
+    if y == 0 :
+        print(x)
+        exit()
+    for i in range(0, 2) :
+        xx = x+dx[i]
+        if 0 <= xx < 10 and l[y][xx] == 1 :
+            l[y][xx] = 0
+            ladder(y, xx, l)
+    if l[y-1][x] == 1 :
+        l[y-1][x] = 0
+        ladder(y-1, x, l)
+
+for i in range(0, 10) :
+    check = 0
+    if l[9][i] == 2 :
+        r = ladder(9, i, l)
