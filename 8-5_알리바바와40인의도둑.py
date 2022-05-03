2f756289758b2bc36813e791781af620e640306a
jinaur <50763720+jinaur@users.noreply.github.com>
2022-05-03 18:08:22 +0900

    .

--- 8-5_알리바바와40인의도둑.py
+++ 8-5_알리바바와40인의도둑.py
@@ -1,36 +1,21 @@
-import sys
-sys.setrecursionlimit(1000000)
 n = int(input())
 
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
 stone_list = []
 check_list = []
-List = []
 for i in range(0, n) :
-    stone = list(map(int, input().split()))
-    check_list.append([0]*n)
-    stone_list.append(stone)
-    List.append([10]*n)
+	stone = list(map(int, input().split()))
+	check_list.append([0]*n)
+	stone_list.append(stone)
 
-Min = 10000000000
-def DFS(x, y, energy) :
-    global Min
-    if energy > Min :
-        return
-    if x == n-1 and y == n-1 :
-        if energy < Min :
-            Min = energy
-        return Min
-    for i in range(0, 4) : # 4방향
-        xx = x+dx[i]
-        yy = y+dy[i]
-        if 0 <= xx < n and 0 <= yy < n and check_list[xx][yy] == 0 :
-            check_list[xx][yy] = 1
-            if List[xx][yy] > energy :
-                List[xx][yy] = energy
-            energy += stone_list[xx][yy]
-            DFS(xx, yy, energy)
+check_list[0][0] = stone_list[0][0]
 
-DFS(0, 0, stone_list[0][0])
-print(Min)
+for i in range(0, n) :
+	check_list[0][i] = check_list[0][i-1]+stone_list[0][i]
+	check_list[i][0] = check_list[i-1][0]+stone_list[i][0]
+	
+for i in range(1, n) :
+	for j in range(1, n) :
+		check_list[i][j] = min(check_list[i-1][j], check_list[i][j-1])+stone_list[i][j]
+
+print(check_list[n-1][n-1])
+
