3d0c40ca75749d08bf7e388afd1b3b1060c54550
jinaur <50763720+jinaur@users.noreply.github.com>
2022-06-03 20:17:45 +0900

    .

--- 7-15_토마토.py
+++ 7-15_토마토.py
@@ -1,25 +1,28 @@
+from collections import deque
 m, n = map(int, input().split())
 
-dx = [-1, 0, 1, 0, 1, -1, 1, -1]
-dy = [0, 1, 0, -1, 1, -1, -1, 1]
-tomato_list = []
-list1 = []
-for i in range(0, n) :
-	tomato = list(map(int, input().split()))
-	tomato_list.append(tomato)
-	for j in range(0, n) :
-		if tomato[j] == 1 :
-			list1.append((i, j))
+matrix = [list(map(int, input().split())) for _ in range(n)]
+queue = deque([])
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+res = 0
 
-def DFS(x, y, j) :
-    for i in range(0, 8) : # 4방향중 안 익은곳으로 감
-        xx = x+dx[i]
-        yy = y+dy[i]
-        if 0 <= xx < m and 0 <= yy < n and tomato_list[xx][yy] == 0 :
-            tomato_list[xx][yy] = j
-            DFS(xx, yy, j+1)
+for i in range(n):
+    for j in range(m):
+        if matrix[i][j] == 1:
+            queue.append([i, j])
 
-for i in range(0, len(list1)) :
-    DFS(list1[i][0], list1[i][1], 1)
+while queue:
+    x, y = queue.popleft()
+    for i in range(4):
+        nx, ny = dx[i] + x, dy[i] + y
+        if 0 <= nx < n and 0 <= ny < m and matrix[nx][ny] == 0:
+            matrix[nx][ny] = matrix[x][y] + 1
+            queue.append([nx, ny])
 
-print(tomato_list)
+for i in matrix:
+    for j in i:
+        if j == 0:
+            print(-1)
+            exit(0)
+    res = max(res, max(i))
+print(res - 1)
