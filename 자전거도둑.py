fe1c3792b8f98fc050d8eef2bafba1314bdcf7a5
jinaur <50763720+jinaur@users.noreply.github.com>
2022-06-13 22:40:46 +0900

    .

---
+++ 자전거도둑.py
@@ -0,0 +1,16 @@
+def main(lock1, lock2) :
+    for i in range(0, 10000) :
+        if lock1 == i and i%2 == 0 :
+            print(1)
+            exit()
+        if lock2 == i and i%2 == 1 :
+            print(1)
+            exit()
+
+    print(0)
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    main(a, b)
+
